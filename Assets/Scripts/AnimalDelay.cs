bc224ece41470ba89035707740b972944692b543
bfranse1 <fran0124@hz.nl>
2018-12-18 12:47:36 +0100

    added delay on animations

---
+++ Assets/Scripts/AnimalDelay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalDelay : MonoBehaviour {
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.Play(0, -1, Random.value);
+
+
+	}
+
+}
