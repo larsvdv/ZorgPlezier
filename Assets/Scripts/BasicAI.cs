18889e49d27818f04d1f2216c698bf5219c1d073
Lars van de Vrede <larsvandevrede@hotmail.com>
2018-12-20 14:28:07 +0100

    Added fish AI

---
+++ Assets/Scripts/BasicAI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicAI : MonoBehaviour
+{
+
+    public Transform[] target;
+    Transform newTarget;
+
+    Animator bassAnimation;
+
+    bool isMoving = false;
+
+    public float speed = 2.0f;
+
+    public bool predator = false;
+
+    void Update()
+    {
+        if (isMoving == false)
+        {
+            newTarget = target[Random.Range(0, target.Length)];
+            isMoving = true;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, newTarget.position, speed * Time.deltaTime);
+
+        if (transform.position == newTarget.position)
+        {
+            isMoving = false;
+        }
+
+        transform.LookAt(newTarget);
+    }
+}
